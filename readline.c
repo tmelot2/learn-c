72894732550c60e45ecd26e6d8f675bd7d15d1d4
tmelot2 <tmelot@gmail.com>
2026-05-19 10:29:59 -0400

    Start on structs

--- readline.c
+++ readline.c
@@ -1,22 +1,29 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* readline.c (Chapter 16, page 395) */
+
+#include <ctype.h>
 #include <stdio.h>
+#include "readline.h"
 
-int read_line(char str[], int n) {
-	int ch, i = 0;
+int read_line(char str[], int n)
+{
+  int ch, i = 0;
 
-	while ((ch = getchar()) != '\n') {
-		if (i < n) {
-			str[i++] = ch;
-		}
-	}
-	str[i] = '\0';
-	return i;
-}
-
-int main(void) {
-	int i = 50;
-	char s[i];
-	printf("Enter a line of text: ");
-	read_line(s, i);
-	printf("String = \"%s\"\n", s);
-	return 0;
+  while (isspace(ch = getchar()))
+    ;
+  while (ch != '\n' && ch != EOF) {
+    if (i < n)
+      str[i++] = ch;
+    ch = getchar();
+  }
+  str[i] = '\0';
+  return i;
 }
